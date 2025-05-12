da73a33d2843b5a012faffa6f0d3746ce5be01fe
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-05-12 11:47:34 +0800

    Basic movement actions.

--- main.ts
+++ main.ts
@@ -1,12 +1,27 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         basic.showIcon(IconNames.Heart)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 75)
         Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
     } else if (receivedNumber == 2) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 75)
         basic.showIcon(IconNames.SmallHeart)
+    } else if (receivedNumber == 12) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
+    } else if (receivedNumber == 14) {
         Maqueen_V5.motorStop(Maqueen_V5.Motors.M1)
         Maqueen_V5.motorStop(Maqueen_V5.Motors.M2)
+    } else if (receivedNumber == 4) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 135)
+    } else if (receivedNumber == 10) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 135)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
+    } else if (receivedNumber == 8) {
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, 100)
+        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 100)
     } else {
         basic.showIcon(IconNames.Angry)
     }
